e5d7a8d5b39b245346224ba64d29015964f08c04
koutskami <koutskami@gmail.com>
2024-05-19 17:09:17 +0200

    zobrazovani null hodnot, nacitani detailu vizitek

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -13,17 +13,15 @@ import java.util.List;
  */
 @Controller
 public class VizitkaController {
-    //Vytvoř si v kontroleru VizitkaController privátní field, který bude obsahovat seznam vizitek (List<Vizitka>) a naplň seznam nějakými údaji. Nezapomeň vytvořit i takové vizitky, které nebudou mít uvedené všechny kontakty.
     private final List<Vizitka> seznamVizitek = List.of(
-            new Vizitka("Dita (Přikrylová) Formánková", "Czechitas z. s.", "Václavské náměstí 837/11", "11000", "dita@czechitas.cs", "+420 800123456", "www.czechitas.cz"),
-            //new Vizitka("Barbora Bühnová", "Czechitas z. s.", "Václavské náměstí 837/11", "11000", null, "+420 800123456", "www.czechitas.cz"),
-            new Vizitka("Monika Ptáčníková", "Czechitas z. s.", "Václavské náměstí 837/11", "11000", "monika@czechitas.cs", "+420 800123456", "www.czechitas.cz"),
-            //new Vizitka("Mirka Zatloukalová", "Czechitas z. s.", "Václavské náměstí 837/11", "11000", "mirka@czechitas.cs", null, "www.czechitas.cz"),
-            new Vizitka("Eliška Krásnohorská", "Firma", "Ulice", "61400", "eliska@eliska.cz", "733333333", "webovka.cz")
+            new Vizitka("Dita (Přikrylová) Formánková", "Czechitas z. s.", "Václavské náměstí 837/11", "11000 Praha 1", "dita@czechitas.cs", "+420 800123456", "www.czechitas.cz"),
+            new Vizitka("Barbora Bühnová", "Czechitas z. s.", "Václavské náměstí 837/11", "11000 Praha 1", null, "+420 800123456", "www.czechitas.cz"),
+            new Vizitka("Monika Ptáčníková", "Czechitas z. s.", "Václavské náměstí 837/11", "11000 Praha 1", "monika@czechitas.cs", "+420 800123456", "www.czechitas.cz"),
+            new Vizitka("Mirka Zatloukalová", "Czechitas z. s.", "Václavské náměstí 837/11", "11000 Praha 1", "mirka@czechitas.cs", null, "www.czechitas.cz"),
+            new Vizitka("Eliška Krásnohorská", "Firma", "Ulice", "61400 Brno-Husovice", "eliska@eliska.cz", "733333333", "webovka.cz"),
+            new Vizitka(null, null, null, null, null, null, null)
     );
-    //Uprav šablonu seznam.html tak, aby zobrazovala vizitky z modelu. - hotovo
-    //TODO Každá vizitka musí odkazovat na svou stránku s detailem.
-    //TODO Upravit šablonu tak aby zvládla nullové hodnoty.
+
     @GetMapping("/")
     public ModelAndView seznam() {
         ModelAndView modelAndView = new ModelAndView("/seznam");
@@ -31,14 +29,10 @@ public class VizitkaController {
         return modelAndView;
     }
 
-    //TODO Uprav metodu detail() tak, aby vracela ModelAndView. Uprav metodu tak, aby z požadavku prohlížeče načítala query parametr id. Zvol správné view a do modelu vlož vizitku získanou ze seznamu na základě id (indexu v seznamu).
-    @GetMapping("/detail")
-    public String detail() {
-        return "detail";
+    @GetMapping("/detail/{index}")
+    public ModelAndView detail(@PathVariable int index) {
+        ModelAndView modelAndView = new ModelAndView("/detail");
+        modelAndView.addObject("vizitka", seznamVizitek.get(index));
+        return modelAndView;
     }
-//    @GetMapping("/detail/{id}")
-//    public ModelAndView detail(@PathVariable int id) {
-//        ModelAndView modelAndView = new ModelAndView("/detail");
-//        modelAndView.addObject("vizitka", seznamVizitek.get(id));
-//        return modelAndView;
 }
